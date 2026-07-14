5a58565978ae26c7905bebd5a9ed9f356751c8cc
PiggyHehe <135648036+PiggyHehe@users.noreply.github.com>
2026-07-14 00:32:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('bot is running!');
+});
+
+// THIS IS THE PART YOU NEED TO ADD:
 app.get('/test-keys', (req, res) => {
   const idPresent = process.env.CLIENT_ID ? "yes" : "no";
   const secretPresent = process.env.CLIENT_SECRET ? "yes" : "no";
   res.send(`client id found: ${idPresent}, client secret found: ${secretPresent}`);
+});
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
 });
